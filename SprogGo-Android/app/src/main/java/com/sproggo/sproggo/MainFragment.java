c939841931eb16f49ba99b58b1b7089d1cfc8218
Davis, Alexander (k1761218) <alexander.davis@kcl.ac.uk>
2018-04-07 23:50:43 +0200

    implemented fragments

--- SprogGo-Android/app/src/main/java/com/sproggo/sproggo/MainFragment.java
+++ SprogGo-Android/app/src/main/java/com/sproggo/sproggo/MainFragment.java
@@ -1,12 +1,14 @@
 package com.sproggo.sproggo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -62,9 +64,16 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+                             Bundle savedInstanceState) {// Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        final Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DescribeActivity.class);
+                MainFragment.this.startActivity(intent);
+            }
+        });
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event

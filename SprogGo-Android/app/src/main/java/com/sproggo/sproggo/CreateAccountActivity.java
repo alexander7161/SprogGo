2fd02912a0e2febbf9cdacfdd82dfad0eca4c2a0
kim <kimbethstonehouse@gmail.com>
2018-04-08 03:42:37 +0100

    create acc links to main

--- SprogGo-Android/app/src/main/java/com/sproggo/sproggo/CreateAccountActivity.java
+++ SprogGo-Android/app/src/main/java/com/sproggo/sproggo/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.sproggo.sproggo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -26,6 +27,10 @@ public class CreateAccountActivity extends AppCompatActivity {
                 if (repeatEmail.equals(email) && repeatPassword.equals(password)) {
                     startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
                     PlayerList.createAccount(username.getText().toString(), password.getText().toString(), email.getText().toString());
+                    SharedPreferences pref = getSharedPreferences("sproggo", 0);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString(username.getText().toString(), password.getText().toString());
+                    editor.commit();
                 }
             }
         });

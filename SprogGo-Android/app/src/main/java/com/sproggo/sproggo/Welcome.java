72caa1a359707f6ae3e2d4888b82d92858e0dea4
kim <kimbethstonehouse@gmail.com>
2018-04-08 01:05:19 +0100

    functionality

--- SprogGo-Android/app/src/main/java/com/sproggo/sproggo/Welcome.java
+++ SprogGo-Android/app/src/main/java/com/sproggo/sproggo/Welcome.java
@@ -2,6 +2,8 @@ package com.sproggo.sproggo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button login = findViewById(R.id.login);
+        Button createAccount = findViewById(R.id.createAccount);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // do this
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // do this
+            }
+        });
     }
 }
